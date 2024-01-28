00b8061e5550f66328a43a54c7ec4443e7a8dcc4
Ryan Tung <53037140+RuizheRyan@users.noreply.github.com>
2024-01-28 13:54:56 +0800

    optimize

--- Assets/Scripts/UI/BalanceDisplay.cs
+++ Assets/Scripts/UI/BalanceDisplay.cs
@@ -11,7 +11,7 @@ public class BalanceDisplay : MonoBehaviour
         float balanceValue = Motorcycle.Instance.BalanceValue;
 
         // 将BalanceValue从-100到100的范围映射到0到1的范围
-        float normalizedBalanceValue = (balanceValue + 50) / 100;
+        float normalizedBalanceValue = (balanceValue + 100) / 200;
 
         // 计算Pin在BalanceBar上的新位置
         float newPositionX = Mathf.Lerp(balanceBar.rect.xMin, balanceBar.rect.xMax, normalizedBalanceValue);
